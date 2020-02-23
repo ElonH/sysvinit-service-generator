1ad43fe11a12b035ff4ca860648ef14697aab471
ElonH <elonhhuang@gmail.com>
2020-02-23 17:25:00 +0800

    fix with shellcheck

--- new-service.sh
+++ new-service.sh
@@ -1,43 +1,43 @@
 #!/bin/bash
 
-SERVICE_FILE=$(tempfile)
+SERVICE_FILE=$(mktemp)
 
 if [ ! -e service.sh ]; then
-  echo "--- Download template ---"
-  echo "I'll now download the service.sh, because is is not downloaded."
-  echo "..."
-  wget -q https://raw.githubusercontent.com/wyhasany/sample-service-script/master/service.sh
+  echo -e "--- Download template ---"
+  echo -e "I'll now download the service.sh, because is is not downloaded."
+  echo -e "..."
+  wget -q https://raw.githubusercontent.com/elonh/sample-service-script/master/service.sh
   if [ "$?" != 0 ]; then
-    echo "I could not download the template!"
-    echo "You should now download the service.sh file manualy. Run therefore:"
-    echo "wget https://raw.githubusercontent.com/wyhasany/sample-service-script/master/service.sh"
+    echo -e "I could not download the template!"
+    echo -e "You should now download the service.sh file manualy. Run therefore:"
+    echo -e "wget https://raw.githubusercontent.com/elonh/sample-service-script/master/service.sh"
     exit 1
   else
-    echo "I donloaded the tmplate sucessfully"
-    echo ""
+    echo -e "I donloaded the tmplate sucessfully"
+    echo -e ""
   fi
 fi
 
 
-echo "--- Copy template ---"
+echo -e "--- Copy template ---"
 cp service.sh "$SERVICE_FILE"
 chmod +x "$SERVICE_FILE"
-echo ""
+echo -e ""
 
-echo "--- Customize ---"
-echo "I'll now ask you some information to customize script"
-echo "Press Ctrl+C anytime to abort."
-echo "Empty values are not accepted."
-echo ""
+echo -e "--- Customize ---"
+echo -e "I'll now ask you some information to customize script"
+echo -e "Press Ctrl+C anytime to abort."
+echo -e "Empty values are not accepted."
+echo -e ""
 
 prompt_token() {
   local VAL=""
   if [ "$3" = "" ]; then
     while [ "$VAL" = "" ]; do
-      echo -n "${2:-$1} : "
-      read VAL
+      echo -e -n "${2:-$1} : "
+      read -e -p -r VAL
       if [ "$VAL" = "" ]; then
-        echo "Please provide a value"
+        echo -e "Please provide a value"
       fi
     done
   else
@@ -46,13 +46,13 @@ prompt_token() {
   VAL=$(printf '%s' "$VAL")
   eval $1=$VAL
   local rstr=$(printf '%q' "$VAL")
-  rstr=$(echo $rstr | sed -e 's/[\/&]/\\&/g') # escape search string for sed http://stackoverflow.com/questions/407523/escape-a-string-for-a-sed-replace-pattern
+  rstr=$(echo -e $rstr | sed -e 's/[\/&]/\\&/g') # escape search string for sed http://stackoverflow.com/questions/407523/escape-a-string-for-a-sed-replace-pattern
   sed -i "s/<$1>/$rstr/g" $SERVICE_FILE
 }
 
 prompt_token 'NAME'        'Service name' $1
 if [ -f "/etc/init.d/$NAME" ]; then
-  echo "Error: service '$NAME' already exists"
+  echo -e "Error: service '$NAME' already exists"
   exit 1
 fi
 
@@ -60,37 +60,37 @@ prompt_token 'DESCRIPTION' ' Description' $2
 prompt_token 'COMMAND'     '     Command' $3
 prompt_token 'USERNAME'    '        User' $4
 if ! id -u "$USERNAME" &> /dev/null; then
-  echo "Error: user '$USERNAME' not found"
+  echo -e "Error: user '$USERNAME' not found"
   exit 1
 fi
 
-echo ""
+echo -e ""
 
-echo "--- Installation ---"
+echo -e "--- Installation ---"
 if [ ! -w /etc/init.d ]; then
-  echo "You didn't give me enough permissions to install service myself."
-  echo "That's smart, always be really cautious with third-party shell scripts!"
-  echo "You should now type those commands as superuser to install and run your service:"
-  echo ""
-  echo "   mv \"$SERVICE_FILE\" \"/etc/init.d/$NAME\""
-  echo "   touch \"/var/log/$NAME.log\" && chown \"$USERNAME\" \"/var/log/$NAME.log\""
-  echo "   update-rc.d \"$NAME\" defaults"
-  echo "   service \"$NAME\" start"
+  echo -e "You didn't give me enough permissions to install service myself."
+  echo -e "That's smart, always be really cautious with third-party shell scripts!"
+  echo -e "You should now type those commands as superuser to install and run your service:"
+  echo -e ""
+  echo -e "   mv \"$SERVICE_FILE\" \"/etc/init.d/$NAME\""
+  echo -e "   touch \"/var/log/$NAME.log\" && chown \"$USERNAME\" \"/var/log/$NAME.log\""
+  echo -e "   update-rc.d \"$NAME\" defaults"
+  echo -e "   service \"$NAME\" start"
 else
-  echo "1. mv \"$SERVICE_FILE\" \"/etc/init.d/$NAME\""
+  echo -e "1. mv \"$SERVICE_FILE\" \"/etc/init.d/$NAME\""
   mv -v "$SERVICE_FILE" "/etc/init.d/$NAME"
-  echo "2. touch \"/var/log/$NAME.log\" && chown \"$USERNAME\" \"/var/log/$NAME.log\""
+  echo -e "2. touch \"/var/log/$NAME.log\" && chown \"$USERNAME\" \"/var/log/$NAME.log\""
   touch "/var/log/$NAME.log" && chown "$USERNAME" "/var/log/$NAME.log"
-  echo "3. update-rc.d \"$NAME\" defaults"
+  echo -e "3. update-rc.d \"$NAME\" defaults"
   update-rc.d "$NAME" defaults
-  echo "4. service \"$NAME\" start"
+  echo -e "4. service \"$NAME\" start"
   service "$NAME" start
 fi
 
-echo ""
-echo "---Uninstall instructions ---"
-echo "The service can uninstall itself:"
-echo "    service \"$NAME\" uninstall"
-echo "It will simply run update-rc.d -f \"$NAME\" remove && rm -f \"/etc/init.d/$NAME\""
-echo ""
-echo "--- Terminated ---"
+echo -e ""
+echo -e "---Uninstall instructions ---"
+echo -e "The service can uninstall itself:"
+echo -e "    service \"$NAME\" uninstall"
+echo -e "It will simply run update-rc.d -f \"$NAME\" remove && rm -f \"/etc/init.d/$NAME\""
+echo -e ""
+echo -e "--- Terminated ---"
